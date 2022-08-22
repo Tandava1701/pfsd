b3fa2faae9b392b615d234e871950ebe5ce459f7
Tandava <tandavakrishna171@gmail.com>
2022-08-22 11:42:25 +0530

    commit done on 22/08/2022

---
+++ Excersise1.2.py
@@ -0,0 +1,14 @@
+class Circle:
+    def __int__(self, r):
+        self.r = r
+
+    def area(self):
+        print(2 * 3.14 * self.r)
+
+    def perimeter(self) -> :
+        print(3.14 * self.r * self.r)
+
+
+c = Circle(20)
+c.area()
+c.perimeter()
